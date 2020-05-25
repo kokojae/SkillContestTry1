23d19107e86a244812f00e38c75b5297ad43562c
kokojae <54296776+kokojae@users.noreply.github.com>
2020-05-25 20:00:36 +0900

    정적 멤버함수 선언
    빌드 오류 수정을 위한 정적 멤버변수 선언

--- Camera.cpp
+++ Camera.cpp
@@ -2,6 +2,11 @@
 #include "Camera.h"
 #include "SkillContestTry1.h"
 
+D3DXVECTOR2 Camera::position = { 0,0 };
+float Camera::degree = 0;
+float Camera::scale = 0;
+D3DXVECTOR2 Camera::shake = { 0,0 };
+
 D3DXMATRIX Camera::GetCameraMatrix()
 {
     D3DXMATRIX mat, matDegree, matScale, matPosition, matScreen;
